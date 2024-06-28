420566e79b9cfe343565d1d048e63f858ccc41ae
ericserka <ericserka@hotmail.com>
2024-06-28 17:04:47 -0300

    changing spectre defaults
    removing ignore case and enabling live_update options by default

--- nvim/lua/plugins/spectre-config.lua
+++ nvim/lua/plugins/spectre-config.lua
@@ -1,3 +1,12 @@
 local spectre = require('spectre')
 
+spectre.setup({
+  live_update = true,
+  default = {
+    find = {
+      cmd = "rg", options = {}
+    }
+  }
+})
+
 vim.keymap.set('n', '<leader>ps', spectre.open, {})
