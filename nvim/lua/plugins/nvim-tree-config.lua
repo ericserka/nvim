926274529300441911f90eca4e1318ac54a975bb
ericserka <ericserka@hotmail.com>
2024-07-12 10:21:07 -0300

    add more options to nvim tree

--- nvim/lua/plugins/nvim-tree-config.lua
+++ nvim/lua/plugins/nvim-tree-config.lua
@@ -1,11 +1,23 @@
-require('nvim-tree').setup {
+require("nvim-tree").setup {
   filters = {
     dotfiles = false
   },
   view = {
-    width = 40
+    width = 40,
+    preserve_window_proportions = false
+  },
+  update_focused_file = {
+    enable = true
+  },
+  actions = {
+    open_file = {
+      resize_window = false
+    }
   }
 }
 
 -- Open filetree
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- Reset filetree to default size
+vim.keymap.set("n", "<leader>rt", require("nvim-tree.view").resize, { noremap = true, silent = true })
