09eba1b2e2f064dffe2a88fb3c05bef5ad600d09
ericserka <ericserka@hotmail.com>
2024-07-13 05:49:03 -0300

    showing file path in lualine

---
+++ nvim/lua/plugins/lualine-config.lua
@@ -0,0 +1,13 @@
+require("lualine").setup(
+  {
+    sections = {
+      lualine_c = {
+        {
+          "filename",
+          file_status = true,
+          path = 1
+        }
+      }
+    }
+  }
+)
